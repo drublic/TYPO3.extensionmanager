219a6d941fda575601a0e8914ebe917c825977bb
Hans Christian Reinl <anything@drublic.de>
2012-08-13 22:14:09 +0200

    Add a grunt file and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,98 @@
+/*globals module */
+module.exports = function (grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+		meta: {
+			banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+			'<%= grunt.template.today("yyyy-mm-dd") %> */'
+		},
+
+		lint: {
+			all: [
+				'Gruntfile.js'
+			]
+		},
+
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				boss: true,
+				eqnull: true,
+				browser: true
+			},
+			globals: {
+				jQuery: true
+			}
+		},
+
+		concat: {
+			deploy: {
+				src: [],
+				dest: ''
+			}
+		},
+
+		rubysass: {
+			dev: {
+				options: {
+					unixNewlines: true,
+					style: 'extended'
+				},
+				files: {
+					'Resources/Public/Css/main.css': 'Resources/Private/Scss/main.scss'
+				}
+			},
+			deploy: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'Resources/Public/Css/main.css': 'Resources/Private/Scss/main.scss'
+				}
+
+			}
+		},
+
+		min: {
+			deploy: {
+				src: ['<config_process:meta.banner>', '<config:concat.deploy.dest>'],
+				dest: ''
+			}
+		},
+
+		watch: {
+			scss: {
+				files: ['Resources/Private/Scss/main.scss', 'Resources/Private/Scss/**/_*.scss'],
+				tasks: 'rubysass:dev'
+			},
+
+			js: {
+				files: '<config:lint.all>',
+				tasks: 'lint'
+			}
+		}
+	});
+
+	// Load some stuff
+	grunt.loadNpmTasks('grunt-sass');
+
+
+	// A task for development
+	grunt.registerTask('dev', 'lint rubysass:dev');
+
+	// A task for deployment
+	grunt.registerTask('deploy', 'lint concat rubysass:deploy min');
+
+	// Default task
+	grunt.registerTask('default', 'lint concat rubysass:dev min');
+
+};
